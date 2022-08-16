746a995b1946c3a30f9c5d29d0e0146f184ffbce
Sai Aditya Guntupalli <adityaguntupalli9999@gmail.com>
2022-08-16 14:19:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,11 +10,11 @@ from streamlit_option_menu import option_menu
 
 # loading the saved models
 
-diabetes_model = pickle.load(open('C:/Users/siddhardhan/Desktop/Multiple Disease Prediction System/saved models/diabetes_model.sav', 'rb'))
-
-heart_disease_model = pickle.load(open('C:/Users/siddhardhan/Desktop/Multiple Disease Prediction System/saved models/heart_disease_model.sav','rb'))
 
 
+diabetes_model = pickle.load(open('C:\Users\adity\OneDrive\Desktop\Hackathon Project\diabetes_model','rb'))
+
+heart_disease_model = pickle.load(open('C:\Users\adity\OneDrive\Desktop\Hackathon Project\heart_disease_model','rb'))
 
 
 # sidebar for navigation
