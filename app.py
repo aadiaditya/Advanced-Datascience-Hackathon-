8cd3afa3ed6eced2a0da41a0ec7b57e3ad3f07a4
Sai Aditya Guntupalli <adityaguntupalli9999@gmail.com>
2022-08-17 11:34:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -45,6 +45,9 @@ if (selected == 'Diabetes Prediction'):
     
     # page title
     st.title('Diabetes Prediction using ML')
+    st.write('Diabetes is a health condition that affects how your body turns food into energy. Most of the food you eat is broken down into sugar (also called glucose) and released into your bloodstream. When your blood sugar goes up, it signals your pancreas to release insulin.Without ongoing, careful management, diabetes can lead to a buildup of sugars in the blood, which can increase the risk of dangerous complications, including stroke and heart disease. So that i decide to predict using Machine Learning in Python.')
+
+
     
     
     # getting the input data from the user
